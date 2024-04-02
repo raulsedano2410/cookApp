ac93f73265e467d42a7ee0e80615616f379b1fc9
sedanomolina <78510879+raulsedano2410@users.noreply.github.com>
2024-04-02 11:15:32 -0500

    .

--- src/view/Customers/Customers.jsx
+++ src/view/Customers/Customers.jsx
@@ -12,89 +12,92 @@ import molinosasociados from '../../assets/images/customers/molinos.svg';
 import nicoll from '../../assets/images/customers/nicoll.svg';
 import pegafan from '../../assets/images/customers/pegafan.png';
 import piramide from '../../assets/images/customers/piramide.png';
-import tqc from '../../assets/images/customers/tqc.webp';
+import tqc from '../../assets/images/customers/tqc.jpg';
 
 const Customers = () => {
     return (
         <div
             id='customers-id'
-            className={styles.customers_container}>
-            <div className={styles.title_container}>
-                <h3>Todos nuestros clientes satisfechos</h3>
-                <span>
-                    Nuestra calidad es importante para que nuestros clientes queden
-                    satisfechos
-                </span>
-            </div>
-            <section className={styles.flags_container}>
-                <div className={styles.flag}>
-                    <img
-                        src={nicoll}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={cobrecon}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={tqc}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={gyplac}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={pegafan}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={metalindustrias}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={molinosasociados}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={`${styles.flag} ${styles.eternit}`}>
-                    <img
-                        src={eternit}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={`${styles.flag} ${styles.gcz}`}>
-                    <img
-                        src={gcz}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={styles.flag}>
-                    <img
-                        src={autoland}
-                        alt='customers flags'
-                    />
-                </div>
-                <div className={`${styles.flag} ${styles.piramide}`}>
-                    <img
-                        src={piramide}
-                        alt='customers flags'
-                    />
-                </div>
+            className={styles.container}>
+            <div className={styles.customers_container}>
 
-            </section>
+                <div className={styles.title_container}>
+                    <h3>Todos nuestros clientes satisfechos</h3>
+                    <span>
+                        Nuestra calidad es importante para que nuestros clientes queden
+                        satisfechos
+                    </span>
+                </div>
+                <section className={styles.flags_container}>
+                    <div className={styles.flag}>
+                        <img
+                            src={nicoll}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={styles.flag}>
+                        <img
+                            src={cobrecon}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.eternit}`}>
+                        <img
+                            src={eternit}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={styles.flag}>
+                        <img
+                            src={gyplac}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.gcz}`}>
+                        <img
+                            src={gcz}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={styles.flag}>
+                        <img
+                            src={metalindustrias}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.molinosasociados}`}>
+                        <img
+                            src={molinosasociados}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.tqc}`}>
+                        <img
+                            src={tqc}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={styles.flag}>
+                        <img
+                            src={pegafan}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.autoland}`}>
+                        <img
+                            src={autoland}
+                            alt='customers flags'
+                        />
+                    </div>
+                    <div className={`${styles.flag} ${styles.piramide}`}>
+                        <img
+                            src={piramide}
+                            alt='customers flags'
+                        />
+                    </div>
+
+                </section>
+            </div>
         </div>
     );
 };
